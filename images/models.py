12cad963a0788447609131f3bb88eb86752c6283
zewa-bamuga <tikhonov.igor2028@yandex.ru>
2024-12-05 16:59:52 +0700

    Second commit

---
+++ images/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Image(models.Model):
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.description
